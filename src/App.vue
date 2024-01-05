cd559d1c807bdb4c65fa6a0afed91a1e3526308e
sjodie1157 <sjodie1157@gmail.com>
2024-01-05 23:29:19 +0200

    added navbar and footer

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> | 
-    <router-link to="/about">About</router-link> |
-    <router-link to="/resume">Resume</router-link>
-  </nav>
+  <NavbarComp/>
   <router-view/>
+  <FooterComp/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import FooterComp from './components/FooterComp.vue';
+import NavbarComp from './components/NavbarComp.vue';
 
-nav {
-  padding: 30px;
+export default {
+  components: {
+    NavbarComp,
+    FooterComp
+    }
 }
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style src="../src/assets/CSS/Styles.css">
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -10,10 +10,27 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('@/views/AboutView.vue')
+  },
+  {
+    path: '/resume',
+    name: 'resume',
+    component: () => import('@/views/ResumeView.vue')
+  },
+  {
+    path: '/projects',
+    name: 'projects',
+    component: () => import('@/views/ProjectsView.vue')
+  },
+  {
+    path: '/review',
+    name: 'review',
+    component: () => import('@/views/ReviewView.vue')
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: () => import('@/views/ContactView.vue')
   }
 ]
 
