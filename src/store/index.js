43e152688fa59859c9c6a8edf3978966c883f1ba
sjodie1157 <sjodie1157@gmail.com>
2024-01-06 23:46:22 +0200

    added home and about page

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,17 @@ const dataUrl = 'https://sjodie1157.github.io/todayPortfolioData/data/'
 
 export default createStore({
   state: {
-    home: null
+    home: null,
+    about: null
   },
   getters: {
   },
   mutations: {
     setHome(state, value) {
       state.home = value
+    },
+    setAbout(state, value) {
+      state.about = value
     }
   },
   actions: {
@@ -17,6 +21,11 @@ export default createStore({
       let res = await fetch(dataUrl)
       let { home } = await res.json()
       context.commit('setHome', home)
+    },
+    async fetchAbout(context) {
+      let res = await fetch(dataUrl)
+      let { about } = await res.json()
+      context.commit('setAbout', about)
     }
   },
   modules: {
