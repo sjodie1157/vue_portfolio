d83aa02bad41a4112639fc910e184e531a23899d
sjodie1157 <sjodie1157@gmail.com>
2024-01-10 00:30:21 +0200

    added style to homeView

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="footer">
-        &copy; Jodie Smith <span id="currYear"></span>
+    <div class="container-fluid">
+        <div class="footer">
+            Jodie Smith &copy;<span id="currYear"> </span>
+        </div>
     </div>
 </template>
 
@@ -23,7 +25,18 @@ export default {
 <style scoped>
 .footer {
     position: fixed;
+    right: 0;
     bottom: 0;
-    margin: 2em;
+    padding: 0 2em;
+    background-color: #009DF5;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    font-weight: 600;
+    transition: all 0.3s;
+}
+.footer:hover {
+    background-color: black;
+    color: #009DF5;
 }
 </style>
